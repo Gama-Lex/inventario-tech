34f9cec14a0e45f14865ec327d3757bc5ef2c635
Gama-Lex <gamaliel.alexisab@gmail.com>
2026-03-12 12:58:45 -0600

    Contectar modelos existentes al admin de Django

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 from .models import Categoria, Producto, Marca, Almacen, Inventario
 
+admin.site.register(Categoria)
+admin.site.register(Marca)
+admin.site.register(Producto)
+admin.site.register(Almacen)
+admin.site.register(Inventario)
